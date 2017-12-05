dc6bdc6aaa809f5eff64930d9aed2b30d4533b1c
beloha <belohadk97@gmail.com>
2017-12-05 22:43:47 +0200

    add book container base html

--- ui-js/src/containers/page/cabinet/addBook/AddBook.jsx
+++ ui-js/src/containers/page/cabinet/addBook/AddBook.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import BookForm from './addBookForm/AddBookForm';
+
 const AddBook = () => {
     
     return (
         <div className='container add-book'>
             Add book popup
+
+            <BookForm />
         </div>
     )
 }
---
+++ ui-js/src/containers/page/cabinet/addBook/addBookForm/AddBookForm.jsx
@@ -0,0 +1,56 @@
+import React, { PureComponent } from 'react';
+
+
+class AddBookForm extends PureComponent {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        
+        return (
+            <div className='container add-book-form'>
+                <h3>
+                    Enter all needed data, thx!
+                </h3>
+
+                <div className='container add-book-form__container'>
+                    <label className='container add-book-form__container_description'> Name </label>
+                    <input className='container add-book-form__container_data-field' type='text' />
+                </div>
+
+                <div className='container add-book-form__container'>
+                    <label className='container add-book-form__container_description'> Category </label>
+                    <input className='container add-book-form__container_data-field' type='text' />
+                </div>
+
+                <div className='container add-book-form__container'>
+                    <label className='container add-book-form__container_description'> Type </label>
+                    <input className='container add-book-form__container_data-field' type='text' />
+                </div>
+
+                <div className='container add-book-form__container'>
+                    <label className='container add-book-form__container_description'> Photo </label>
+                    <input className='container add-book-form__container_data-field' type='text' />
+                </div>
+
+                <div className='container add-book-form__container'>
+                    <label className='container add-book-form__container_description'> Price </label>
+                    <input className='container add-book-form__container_data-field' type='text' />
+                </div>
+
+                <div className='container add-book-form__container add-book-form__container_control'>
+                    <button className='add-book-form__container_button add-book-form__container_button-reset'>
+                        reset
+                    </button>
+                    <button className='add-book-form__container_button add-book-form__container_button-add'>
+                        add book
+                    </button>
+                </div>
+
+            </div>
+        )
+    }
+}
+
+export default AddBookForm;
