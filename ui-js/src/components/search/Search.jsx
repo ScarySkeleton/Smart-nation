4d0dbf7505ed8ed108382dd4d27df7938c086c83
beloha <belohadk97@gmail.com>
2017-12-10 01:58:42 +0200

    search refactor

--- ui-js/src/components/search/Search.jsx
+++ ui-js/src/components/search/Search.jsx
@@ -1,25 +1,25 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 
 import './search.scss';
 
-class Search extends PureComponent {
-    render() {
-        return (
-				
-				<div className='container search'>
-					<div className='search-title'>
-						<h3 >Find your dream book</h3>
-					</div>
-					<div className='search-form'>
-						<div className="search-content">
-							<input type="text" name="title" placeholder="TITLE"/>
-							<input type="text" name="author" placeholder="AUTHOR"/>
-							<button type="button">SEARCH</button>
-						</div>
-					</div>
-				</div>
-        );
-    };
-};
+const Search = props => (
+	<div className='container search'>
+
+		<div className='search-title'>
+			<h3 >Find your dream book</h3>
+		</div>
+
+		<div className='search-form'>
+			<div className="search-content">
+				<input type="text" name="title" placeholder="TITLE"/>
+				<input type="text" name="author" placeholder="AUTHOR"/>
+				<button 
+					type="button"
+					onClick={this.props.clickToSearch}>SEARCH</button>
+			</div>
+		</div>
+
+	</div>
+);
 
 export default Search;
--- ui-js/src/containers/page/home/search/Search.jsx
+++ ui-js/src/containers/page/home/search/Search.jsx
@@ -3,9 +3,20 @@ import React, { PureComponent } from 'react';
 import Search from '../../../../components/search/Search.jsx';
 
 class SearchContainer extends PureComponent {
+    constructor(props) {
+        super(props);
+        
+        this.search = this.search.bind(this);
+    }
+
+    search() {
+        console.log(search);
+    }
+
     render() {
         return ( 
-            <Search />
+            <Search
+            clickToSearch={this.search} />
         );
     };
 };
