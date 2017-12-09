344f7afff1428a43ae103654d0acc8f9bcb64fb0
Vladimir Kovalenko <proladge@gmail.com>
2017-12-10 01:53:06 +0200

    GetAllBooks fixed

---
+++ server/BookSender/Models/BookOnSchelf.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace BookSender.Models
+{
+	public class BookOnShelf
+    {
+		public int Id { get; set; }
+
+		public int? CurrentUserId { get; set; }
+
+		public string Title { get; set; }
+
+		public int? GenreId { get; set; }
+
+		public string Author { get; set; }
+
+		public string Description { get; set; }
+
+		public int? ConributorId { get; set; }
+
+		public DateTime CreatedOn { get; set; }
+
+		public DateTime PrintedOn { get; set; }
+
+		public string ISBN { get; set; }
+
+		public string AmazonId { get; set; }
+
+		public decimal? Price { get; set; }
+
+		public int? PictureId { get; set; }
+
+		public bool IsUsable { get; set; }
+
+	}
+}
