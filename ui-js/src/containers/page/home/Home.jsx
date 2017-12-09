5b16a50fcf3419ef04061802111e408bc438e603
beloha <belohadk97@gmail.com>
2017-12-10 01:51:24 +0200

    google page: a little bit of hardcoding

--- ui-js/src/containers/page/home/Home.jsx
+++ ui-js/src/containers/page/home/Home.jsx
@@ -2,6 +2,11 @@ import React, { PureComponent } from 'react';
 import Search from './search/Search.jsx';
 
 import './home.scss';
+// Google map
+import GoogleMaps from './googleMap/googleMap';
+import GoogleMapLoading from './googleMap/googleMapLoading';
+import GoogleMapContainer from './googleMap/googleMapContainer';
+import GoogleMapElement from './googleMap/googleMapElement';
 
 class HomeContainer extends PureComponent {
      constructor(props) {
@@ -12,6 +17,18 @@ class HomeContainer extends PureComponent {
         return (
             <div className='container home'>
               <Search />
+              <GoogleMaps
+                googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+                loadingElement={<div style={{ height: `100%` }} />}
+                containerElement={<div style={{ height: `400px` }} />}
+                mapElement={<div style={{ height: `100%` }} />} 
+                />
+                {/* <GoogleMaps
+                googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+                loadingElement={<GoogleMapLoading />}
+                containerElement={<GoogleMapContainer />}
+                mapElement={<GoogleMapElement />}
+                /> */}
             </div>
         )
     }
