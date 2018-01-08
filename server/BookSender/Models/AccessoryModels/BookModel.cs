ca6506b6ee1f70d07016ba573a146b27d0751dbd
Andrii Davydiuk <mr.wokawoka@gmail.com>
2018-01-08 20:28:59 +0200

    fixed book adding

--- server/BookSender/Models/AccessoryModels/BookModel.cs
+++ server/BookSender/Models/AccessoryModels/BookModel.cs
@@ -7,14 +7,14 @@ namespace BookSender.Models.AccessoryModels
 {
 	public class BookModel
 	{
-		public string name { get; set; }
+        public string name { get; set; }
 		public string author { get; set; }
-		public int type { get; set; }
-		public int genre { get; set; }
+		public int? type { get; set; }
+		public int? genre { get; set; }
 		public string photo { get; set; }
 		public string photoInBinary { get; set; }
-		public int price { get; set; }
-		public string AltitudeCoordinate { get; set; }
-		public string LongtiudeCoordinate { get; set; }
-	}
+		public int? price { get; set; }
+        public string AltitudeCoordinat { get; set; }
+        public string LongtiudeCoordinate { get; set; }
+    }
 }
