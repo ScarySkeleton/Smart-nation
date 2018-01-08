bb6101be2313299609be4f2d421701afed1604d9
beloha <belohadk97@gmail.com>
2018-01-08 15:54:18 +0200

    working on ordering book

--- ui-js/src/containers/book/orderBook/OrderBook.jsx
+++ ui-js/src/containers/book/orderBook/OrderBook.jsx
@@ -3,6 +3,7 @@ import {withRouter} from 'react-router';
 import {connect} from 'react-redux';
 
 import './orderBook.scss';
+import defBookImg from 'img/cabinet/book.png'
 import {orderBookRequest} from './orderBook.action';
 
 class OrderBook extends PureComponent {
@@ -11,13 +12,36 @@ class OrderBook extends PureComponent {
     }
 
     componentDidMount() {
-        this.props.getOrderBookData({id: this.props.match.params.id});
+        //this.props.getOrderBookData({id: this.props.match.params.id});
     }
 
     render() {
+        console.log(this.props.bookData);
+        const data = this.props.bookData;
         return (
             <div className='container book-order'>
-                order book
+                <div className='book-order__image'>
+                    {
+                        data.image !== null 
+                        ? <img className='book-order__image_img' 
+                            src={data.image} 
+                            alt={`${data.title}-${data.author}-${data.genre}`} />
+                        : <img className='book-order__image_img' 
+                            src={defBookImg} 
+                            alt={`${data.title}-${data.author}-${data.genre}`} />
+                    }
+                </div>
+                <div className='book-order__title'>
+                    <h3 className='book-order__title_text'>
+                        Title: <span className='bold'>{data.title}</span>
+                    </h3>
+                </div>
+                <div className='book-order__author'>
+                    <h3 className='book-order__author_text'>
+                        Author: <span className='bold'>{data.author}</span>
+                    </h3>
+                </div>
+
             </div>
         )
     }
